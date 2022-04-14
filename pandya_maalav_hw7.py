8d439da15d39d45e59ced2b70bdf70a087517d98
Maalav Pandya <mpandya1@c0710a-s30.ufhpc>
2022-04-14 12:04:48 -0400

    Last commit before trying pandas

--- pandya_maalav_hw7.py
+++ pandya_maalav_hw7.py
@@ -4,10 +4,23 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt("sed.txt", delimiter = ",")
 
-x = data[:, 0]
-y = data[:, 1]
+wv = data[:, 0] #Wavelength
+lm = data[:, 1] #Luminosity
 
-plt.plot(x, y)
+x = np.where((wv > 10) & (wv < 1000))
+
+tempOne  = x[0] #Returns an array
+tempTwo = x[-1]
+
+lowerBound = tempOne[0]
+upperBound = tempTwo[-1]
+integral = np.trapz(np.flip(tempTwo), np.flip(tempOne))
+
+print(lowerBound)
+print(upperBound)
+print(integral)
+
+plt.plot(wv, lm)
 plt.xscale("log")
 plt.yscale("log")
 
